05c4864d74c4927f31b91f3a7e0f49fb2ed2951c
Sergey Makarichev <harleymack@mail.ru>
2025-02-06 09:58:11 +0300

    homework 2 done

--- src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
+++ src/main/scala/ru/otus/module1/DataCollection1/homework/homework1.scala
@@ -1,18 +1,22 @@
 package ru.otus.module1.DataCollection1
-/*
+
+import scala.util.Random
+
 class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
-  }
+	def isFirstBlackSecondWhite: Boolean = {
+		val urn: List[Int] = List(1, 1, 1, 0, 0, 0)
+		val drawn: List[Int] = Random.shuffle(urn).take(2)
+		drawn.contains(1)
+	}
 }
 
 object BallsTest {
-  def main(args: Array[String]): Unit = {
-    val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
-    val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
-    println(countOfPositiveExperiments / count)
-  }
-}*/
+	def main(args: Array[String]): Unit = {
+		val count = 1000000
+		val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+		val countOfExperiments: List[Boolean] = listOfExperiments.map(_.isFirstBlackSecondWhite)
+		val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
+		println(countOfPositiveExperiments / count)
+	}
+}
